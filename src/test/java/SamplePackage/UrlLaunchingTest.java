ff4ba9749684ef43f435e5ca769872adffcd7995
Rajitha <rajitha220@gmail.com>
2018-01-30 17:39:02 +0530

    Maven Project

---
+++ src/test/java/SamplePackage/UrlLaunchingTest.java
@@ -0,0 +1,11 @@
+package SamplePackage;
+
+import org.testng.annotations.Test;
+
+public class UrlLaunchingTest {
+
+  @Test
+  public void main() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
